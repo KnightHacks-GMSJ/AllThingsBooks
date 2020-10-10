c5b4e568d1ed75dd39703f8682a45a8cc4123494
Grant Woodruff <grant.woodruff20@gmail.com>
2020-10-10 18:12:49 -0400

    Final-1 of title page

--- components/Title.js
+++ components/Title.js
@@ -1,17 +1,39 @@
-import { StatusBar } from "expo-status-bar";
-//import * as Font from "expo-font";
-//import { AppLoading } from "expo";
-import React from "react";
-import { Text } from "react-native";
-
-//const fetchFonts = () => {
-//return Font.loadAsync({
-//"architectsdaughter-regular": require("./assets/fonts/ArchitectsDaughter-Regular.ttf"),
-//});
-//};
-
-function Title() {
-  return <Text>AllThingsBooks</Text>;
+//import { StatusBar } from "expo-status-bar";
+import * as Font from "expo-font";
+//import React from "react";
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { AppLoading } from "expo";
+import { processFontFamily, useFonts } from "expo-font";
+import { Inter_400Regular } from "@expo-google-fonts/inter";
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "ArchitectsDaughter-Regular": require("../assets/fonts/ArchitectsDaughter-Regular.ttf"),
+  });
+};
+function Title(props) {
+  const [dataloaded, setDataLoaded] = useState(false);
+  if (!dataloaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    );
+  }
+  return (
+    <View>
+      <Text style={styles.title}>All Things Books</Text>
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  title: {
+    justifyContent: "center",
+    marginTop: "60%",
+    fontFamily: "ArchitectsDaughter-Regular",
+    fontSize: 45,
+  },
+});
 export default Title;
