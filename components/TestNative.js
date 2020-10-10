e323a89248bce8a181d822e42df3aaaf93d91330
Mujahid Jaffer <mujahidrj@gmail.com>
2020-10-10 15:37:36 -0400

    add something in test comp

--- components/TestNative.js
+++ components/TestNative.js
@@ -1,18 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Button, TextInput, View, StyleSheet } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, Text, TextInput, View, StyleSheet } from 'react-native';
 
 function TestNative() {
-    return (
-       
-    );
+	return <Text>Hello</Text>;
 }
 
 const style = StyleSheet.create({
-    searchContainer: {},
-    inputContainer: {
-        borderWidth: 1,
-    },
+	searchContainer: {},
+	inputContainer: {
+		borderWidth: 1,
+	},
 });
 
 export default TestNative;
