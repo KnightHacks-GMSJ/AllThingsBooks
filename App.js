06c1b312c0be2538626fdb5f66e21834f9ca479d
stefanwerleman <stefanwerleman@knights.ucf.edu>
2020-10-10 15:26:19 -0400

    native-test

--- App.js
+++ App.js
@@ -1,33 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, ImageBackground, View, SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, ImageBackground, View, SafeAreaView } from "react-native";
+import Title from "./components/Title";
+import Search from "./components/Search";
+import BookList from "./components/BookList";
 
-import Title from './components/Title';
-import Search from './components/Search';
-import BookList from './components/BookList';
+import { Container, Card } from "native-base";
 
 function App() {
-	return (
-		<ImageBackground
-			style={styles.backgroundImage}
-			source={require('./assets/background-image.jpg')}
-		>
-			<SafeAreaView style={styles.safeArea}>
-				<Title />
-				<Search />
-				<BookList />
-			</SafeAreaView>
-		</ImageBackground>
-	);
+    return (
+        <Container>
+            <ImageBackground
+                style={styles.backgroundImage}
+                source={require("./assets/background-image.jpg")}
+            >
+                <SafeAreaView style={styles.safeArea}>
+                    <Title />
+                    <Search />
+                    <Card>
+                        <BookList />
+                    </Card>
+                </SafeAreaView>
+            </ImageBackground>
+        </Container>
+    );
 }
 
 const styles = StyleSheet.create({
-	backgroundImage: {
-		flex: 1,
-	},
-	safeArea: {
-		alignItems: 'center',
-	},
+    backgroundImage: {
+        flex: 1,
+    },
+    safeArea: {
+        alignItems: "center",
+    },
 });
 
 export default App;
--- components/Book.js
+++ components/Book.js
@@ -2,6 +2,9 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View, Text } from "react-native";
 
+// This will be a book object that will contain:
+// => Title, Author, publisher, date, description, page count, rating, and linktobuy
+
 function Book() {
     return (
         <View>
