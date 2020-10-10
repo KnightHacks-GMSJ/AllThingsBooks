52efb96f2248831577bb41b08777c82997f2feb1
Mujahid Jaffer <mujahidrj@gmail.com>
2020-10-10 02:47:25 -0400

    test

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import SampleComponent from './SampleComponent';
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+			<Text>Open up App.js to start working on your app</Text>
 			<SampleComponent />
 			<StatusBar style="auto" />
 		</View>
