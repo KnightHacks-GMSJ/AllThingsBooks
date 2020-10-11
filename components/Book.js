ee83adfaba9d6109a0e260cbae111bd3d9e32b8f
Grant Woodruff <grant.woodruff20@gmail.com>
2020-10-11 01:12:27 -0400

    Fixed card height

--- components/Book.js
+++ components/Book.js
@@ -1,35 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Image } from "react-native";
 
-import { Card, Container, Content, Text, Header, CardItem, Body, Right } from 'native-base';
+import {
+  Card,
+  Container,
+  Content,
+  Text,
+  Header,
+  CardItem,
+  Body,
+  Right,
+} from "native-base";
 
 function Book({ book }) {
-	return (
-		<Container
-			style={{
-				padding: 20,
-				marginVertical: 20,
-				backgroundColor: '#03BBF9',
-				borderRadius: 30,
-				borderWidth: 0,
-				marginVertical: 5,
-			}}
-		>
-			<Content>
-				<View>
-					<Body>
-						<Text>{book.title}</Text>
-						<Text>{book.author}</Text>
-						<Text>{book.publisher}</Text>
-						<Text>{book.publishedDate}</Text>
-						<Text>{book.linkToBuy}</Text>
-						<Text>{book.pageCount} Pages</Text>
-					</Body>
-				</View>
-			</Content>
-		</Container>
-	);
+  return (
+    <Container
+      style={{
+        padding: 15,
+        height: 150,
+        backgroundColor: "#03BBF9",
+        borderRadius: 30,
+        borderWidth: 0.5,
+        marginVertical: 5,
+        marginHorizontal: -20,
+      }}
+    >
+      <Content>
+        <View>
+          <Body>
+            <Text>{book.title}</Text>
+            <Text>{book.author}</Text>
+            <Text>{book.publisher}</Text>
+            <Text>{book.publishedDate}</Text>
+            <Text>{book.linkToBuy}</Text>
+            <Text>{book.pageCount} Pages</Text>
+          </Body>
+        </View>
+      </Content>
+    </Container>
+  );
 }
 
 export default Book;
--- components/BookList.js
+++ components/BookList.js
@@ -1,29 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
 
-import Book from './Book';
-import { Container, Content, List, ListItem, Row, Text } from 'native-base';
+import Book from "./Book";
+import { Container, Content, List, ListItem, Row, Text } from "native-base";
 
 function BookList(props) {
-	return (
-		<Container
-			style={{
-				width: '75%',
-				marginVertical: 20,
-				flex: 1,
-				flexDirection: 'column',
-				justifyContent: 'space-between',
-				flexGrow: 20,
-				alignContent: 'space-between',
-				backgroundColor: 'transparent',
-			}}
-		>
-			{props.books.map((book) => (
-				<Book key={book.id} book={book} />
-			))}
-		</Container>
-	);
+  return (
+    <Container
+      style={{
+        width: "75%",
+        height: "-5%",
+        marginVertical: 20,
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "space-between",
+        flexGrow: 20,
+        alignContent: "space-between",
+        backgroundColor: "transparent",
+      }}
+    >
+      {props.books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </Container>
+  );
 }
 
 export default BookList;
