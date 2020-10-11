ee7e3798f10b06d3036990adf4ed8dba10495a7c
Mujahid Jaffer <mujahidrj@gmail.com>
2020-10-10 20:47:22 -0400

    Updated Title

--- components/Title.js
+++ components/Title.js
@@ -1,9 +1,32 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { Text } from "react-native";
-
-function Title() {
-    return <Text>AllThingsBooks</Text>;
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { AppLoading } from 'expo';
+import { processFontFamily, useFonts } from 'expo-font';
+import { Inter_400Regular } from '@expo-google-fonts/inter';
+const fetchFonts = () => {
+	return Font.loadAsync({
+		'ArchitectsDaughter-Regular': require('../assets/fonts/ArchitectsDaughter-Regular.ttf'),
+	});
+};
+function Title(props) {
+	const [dataloaded, setDataLoaded] = useState(false);
+	if (!dataloaded) {
+		return <AppLoading startAsync={fetchFonts} onFinish={() => setDataLoaded(true)} />;
+	}
+	return (
+		<View>
+			<Text style={styles.title}>All Things Books</Text>
+		</View>
+	);
 }
 
+const styles = StyleSheet.create({
+	title: {
+		justifyContent: 'center',
+		marginTop: '60%',
+		fontFamily: 'ArchitectsDaughter-Regular',
+		fontSize: 45,
+	},
+});
 export default Title;
