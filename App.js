dc96c4470044d68679104cd5ec0bd374293645c8
stefanwerleman <stefanwerleman@knights.ucf.edu>
2020-10-11 01:53:45 -0400

    priming book pages

--- App.js
+++ App.js
@@ -1,65 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, ImageBackground, View, SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, ImageBackground, View, SafeAreaView } from "react-native";
 
-import Title from './components/Title';
-import Search from './components/Search';
-import BookList from './components/BookList';
-import { Container, Content, Header } from 'native-base';
+import Title from "./components/Title";
+import Search from "./components/Search";
+import BookList from "./components/BookList";
+import { Container, Content, Header } from "native-base";
 
 function App() {
-	const [currentBooksList, setCurrentBooksList] = useState([]);
+    const [currentBooksList, setCurrentBooksList] = useState([]);
 
-	const onSearch = (booksArray) => {
-		setCurrentBooksList(booksArray);
-		console.log(currentBooksList);
-	};
+    const onSearch = (booksArray) => {
+        setCurrentBooksList(booksArray);
+        console.log(currentBooksList);
+    };
 
-	const books = [
-		{
-			id: 1,
-			title: 'A Game of Thrones',
-			author: 'George R.R. Martin',
-			publisher: 'Sample Publisher',
-			publishedDate: '02/02/1999',
-			description:
-				'Minim ad fugiat eiusmod eiusmod magna occaecat ipsum do esse laboris. Occaecat nulla deserunt minim et exercitation duis aliquip velit consectetur amet consectetur. Eu officia exercitation do quis eiusmod reprehenderit dolor ea occaecat cillum id non.',
-			pageCount: 800,
-			linkToBuy: 'https://heresalink.com/buythisbookitsreallyreallyreallygood',
-		},
-		{ id: 2, title: 'A Game of Thrones', author: 'George R.R. Martin' },
-		// { id: 3, title: 'A Game of Thrones', author: 'George R.R. Martin' },
-		// { id: 4, title: 'A Game of Thrones', author: 'George R.R. Martin' },
-		// { id: 5, title: 'A Game of Thrones', author: 'George R.R. Martin' },
-		// { id: 6, title: 'A Game of Thrones', author: 'George R.R. Martin' },
-		// { id: 7, title: 'A Game of Thrones', author: 'George R.R. Martin' },
-	];
-
-	return (
-		<Container>
-			<ImageBackground
-				style={styles.backgroundImage}
-				source={require('./assets/background-image.jpg')}
-			>
-				<Content>
-					<SafeAreaView style={styles.safeArea}>
-						<Title booksArray={currentBooksList} />
-						<Search onSearch={onSearch} />
-						<BookList books={books} />
-					</SafeAreaView>
-				</Content>
-			</ImageBackground>
-		</Container>
-	);
+    return (
+        <Container>
+            <ImageBackground
+                style={styles.backgroundImage}
+                source={require("./assets/background-image.jpg")}
+            >
+                <Content>
+                    <SafeAreaView style={styles.safeArea}>
+                        <Title booksArray={currentBooksList} />
+                        <Search onSearch={onSearch} />
+                        <BookList books={currentBooksList} />
+                    </SafeAreaView>
+                </Content>
+            </ImageBackground>
+        </Container>
+    );
 }
 
 const styles = StyleSheet.create({
-	backgroundImage: {
-		flex: 1,
-	},
-	safeArea: {
-		alignItems: 'center',
-	},
+    backgroundImage: {
+        flex: 1,
+    },
+    safeArea: {
+        alignItems: "center",
+    },
 });
 
 export default App;
--- components/Book.js
+++ components/Book.js
@@ -1,38 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Modal, TouchableHighlight, View } from "react-native";
 
-import { Card, Container, Content, Text, Header, CardItem, Body, Right } from 'native-base';
+import {
+    Card,
+    Container,
+    Content,
+    Text,
+    Header,
+    CardItem,
+    Body,
+    Button,
+    Right,
+} from "native-base";
 
-function Book({ book }) {
-	return (
-		<Container
-			style={{
-				padding: 20,
-				backgroundColor: '#03BBF9',
-				borderRadius: 30,
-				borderWidth: 0,
-				marginVertical: 5,
-			}}
-		>
-			<Content>
-				<Card transparent>
-					<Body>
-						<Text>{book.title}</Text>
-						<Text>{book.author}</Text>
-						<Text>{book.publisher}</Text>
-						<Text>{book.publishedDate}</Text>
-						<Text>{book.linkToBuy}</Text>
-						<Text>{book.pageCount} Pages</Text>
-					</Body>
-					<CardItem cardBody bordered style={{ paddingBottom: 100, margin: 30 }}>
-						<Image source={require('../assets/content.jpg')} style={{ height: 200, flex: 1 }} />
-					</CardItem>
-					<Body></Body>
-				</Card>
-			</Content>
-		</Container>
-	);
+function Book({ book, onModal }) {
+    return (
+        <TouchableHighlight
+            style={{
+                padding: 20,
+                height: "12%",
+                width: "100%",
+                backgroundColor: "white",
+                borderRadius: 30,
+                marginVertical: 5,
+            }}
+            onPress={() => onModal(true)}
+        >
+            <Content>
+                <Card transparent>
+                    <Body>
+                        <Text>{book.title}</Text>
+                        <Text note>{book.author}</Text>
+                    </Body>
+                </Card>
+            </Content>
+        </TouchableHighlight>
+    );
 }
 
 export default Book;
