84672ffe62bd615d8e897618712c748aeea1df4b
Mujahid Jaffer <mujahidrj@gmail.com>
2020-10-10 23:25:08 -0400

    FINALLY THERES SPACE

--- App.js
+++ App.js
@@ -1,57 +1,55 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, ImageBackground, View, SafeAreaView } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, ImageBackground, View, SafeAreaView } from 'react-native';
 
-import Title from "./components/Title";
-import Search from "./components/Search";
-import BookList from "./components/BookList";
-import { Container, Content, Header } from "native-base";
+import Title from './components/Title';
+import Search from './components/Search';
+import BookList from './components/BookList';
+import { Container, Content, Header } from 'native-base';
 
 function App() {
-    const [currentBooksList, setCurrentBooksList] = useState([]);
+	const [currentBooksList, setCurrentBooksList] = useState([]);
 
-    const onSearch = (booksArray) => {
-        setCurrentBooksList(booksArray);
-        console.log(currentBooksList);
-    };
+	const onSearch = (booksArray) => {
+		setCurrentBooksList(booksArray);
+		console.log(currentBooksList);
+	};
 
-    const books = [
-        { id: 1, title: "A Game of Thrones", author: "George R.R. Martin" },
-        // { id: 2, title: "A Game of Thrones", author: "George R.R. Martin" },
-        // { id: 3, title: "A Game of Thrones", author: "George R.R. Martin" },
-        // { id: 4, title: "A Game of Thrones", author: "George R.R. Martin" },
-        // { id: 5, title: "A Game of Thrones", author: "George R.R. Martin" },
-        // { id: 6, title: "A Game of Thrones", author: "George R.R. Martin" },
-        // { id: 7, title: "A Game of Thrones", author: "George R.R. Martin" },
-    ];
+	const books = [
+		{ id: 1, title: 'A Game of Thrones', author: 'George R.R. Martin' },
+		{ id: 2, title: 'A Game of Thrones', author: 'George R.R. Martin' },
+		// { id: 3, title: 'A Game of Thrones', author: 'George R.R. Martin' },
+		// { id: 4, title: 'A Game of Thrones', author: 'George R.R. Martin' },
+		// { id: 5, title: 'A Game of Thrones', author: 'George R.R. Martin' },
+		// { id: 6, title: 'A Game of Thrones', author: 'George R.R. Martin' },
+		// { id: 7, title: 'A Game of Thrones', author: 'George R.R. Martin' },
+	];
 
-    // <ImageBackground
-    // style={styles.backgroundImage}
-    // source={require("./assets/background-image.jpg")}
-    // >
-    // </ImageBackground>
-
-    return (
-        <Container>
-            <Header></Header>
-            <Content>
-                <SafeAreaView style={styles.safeArea}>
-                    <Title />
-                    <Search onSearch={onSearch} />
-                    <BookList books={books} />
-                </SafeAreaView>
-            </Content>
-        </Container>
-    );
+	return (
+		<Container>
+			<ImageBackground
+				style={styles.backgroundImage}
+				source={require('./assets/background-image.jpg')}
+			>
+				<Content>
+					<SafeAreaView style={styles.safeArea}>
+						<Title />
+						<Search onSearch={onSearch} />
+						<BookList books={books} />
+					</SafeAreaView>
+				</Content>
+			</ImageBackground>
+		</Container>
+	);
 }
 
 const styles = StyleSheet.create({
-    backgroundImage: {
-        flex: 1,
-    },
-    safeArea: {
-        alignItems: "center",
-    },
+	backgroundImage: {
+		flex: 1,
+	},
+	safeArea: {
+		alignItems: 'center',
+	},
 });
 
 export default App;
--- components/Book.js
+++ components/Book.js
@@ -1,23 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View, Image } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Image } from 'react-native';
 
-import { Card, Container, Content, Text, Header, CardItem } from "native-base";
+import { Card, Container, Content, Text, Header, CardItem, Body, Right } from 'native-base';
 
 function Book(props) {
-    return (
-        <Container>
-            <Card>
-                <Header>
-                    <Text>{props.book.title}</Text>
-                    <Text note>{props.book.author}</Text>
-                </Header>
-                <CardItem cardBody>
-                    <Image source={require("../assets/content.jpg")} />
-                </CardItem>
-            </Card>
-        </Container>
-    );
+	return (
+		<Container
+			style={{ padding: 20, backgroundColor: 'dodgerblue', borderWidth: 0, marginVertical: 5 }}
+		>
+			<Content>
+				<Card transparent>
+					<Body>
+						<Text>{props.book.title}</Text>
+						<Text note>{props.book.author}</Text>
+					</Body>
+					<CardItem cardBody bordered style={{ margin: 30 }}>
+						<Image source={require('../assets/content.jpg')} style={{ height: 400, flex: 1 }} />
+					</CardItem>
+				</Card>
+			</Content>
+		</Container>
+	);
 }
 
 export default Book;
