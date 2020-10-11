d7f7f1447e491619d8b1db17ceb38d309ac68195
Mujahid Jaffer <mujahidrj@gmail.com>
2020-10-10 23:52:24 -0400

    Include dummy data in book component

--- components/Book.js
+++ components/Book.js
@@ -7,17 +7,28 @@ import { Card, Container, Content, Text, Header, CardItem, Body, Right } from 'n
 function Book({ book }) {
 	return (
 		<Container
-			style={{ padding: 20, backgroundColor: 'dodgerblue', borderWidth: 0, marginVertical: 5 }}
+			style={{
+				padding: 20,
+				backgroundColor: '#03BBF9',
+				borderRadius: 30,
+				borderWidth: 0,
+				marginVertical: 5,
+			}}
 		>
 			<Content>
 				<Card transparent>
 					<Body>
 						<Text>{book.title}</Text>
-						<Text note>{book.author}</Text>
+						<Text>{book.author}</Text>
+						<Text>{book.publisher}</Text>
+						<Text>{book.publishedDate}</Text>
+						<Text>{book.linkToBuy}</Text>
+						<Text>{book.pageCount} Pages</Text>
 					</Body>
-					<CardItem cardBody bordered style={{ margin: 30 }}>
-						<Image source={require('../assets/content.jpg')} style={{ height: 400, flex: 1 }} />
+					<CardItem cardBody bordered style={{ paddingBottom: 100, margin: 30 }}>
+						<Image source={require('../assets/content.jpg')} style={{ height: 200, flex: 1 }} />
 					</CardItem>
+					<Body></Body>
 				</Card>
 			</Content>
 		</Container>
