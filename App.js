fb3e3c8fc064bd8884bd90d3e922a3b526b0de8c
stefanwerleman <stefanwerleman@knights.ucf.edu>
2020-10-10 21:13:08 -0400

    Fixed missing useState import

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, ImageBackground, View, SafeAreaView } from "react-native";
 
 import Title from "./components/Title";
@@ -8,35 +8,35 @@ import BookList from "./components/BookList";
 import { Container } from "native-base";
 
 function App() {
-	const [currentBooksList, setCurrentBooksList] = useState([]);
+    const [currentBooksList, setCurrentBooksList] = useState([]);
 
-	const onSearch = (booksArray) => {
-		setCurrentBooksList(booksArray);
-		console.log(currentBooksList);
-	};
-  return (
-		<Container>
-			<ImageBackground
-				style={styles.backgroundImage}
-				source={require("./assets/background-image.jpg")}
-			>
-				<SafeAreaView style={styles.safeArea}>
-					<Title />
-					<Search onSearch={onSearch} />
-					<BookList />
-				</SafeAreaView>
-			</ImageBackground>
-		</Container>
-  );
+    const onSearch = (booksArray) => {
+        setCurrentBooksList(booksArray);
+        console.log(currentBooksList);
+    };
+    return (
+        <Container>
+            <ImageBackground
+                style={styles.backgroundImage}
+                source={require("./assets/background-image.jpg")}
+            >
+                <SafeAreaView style={styles.safeArea}>
+                    <Title />
+                    <Search onSearch={onSearch} />
+                    <BookList />
+                </SafeAreaView>
+            </ImageBackground>
+        </Container>
+    );
 }
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-  },
-  safeArea: {
-    alignItems: "center",
-  },
+    backgroundImage: {
+        flex: 1,
+    },
+    safeArea: {
+        alignItems: "center",
+    },
 });
 
 export default App;
