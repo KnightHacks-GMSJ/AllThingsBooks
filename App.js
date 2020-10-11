d7c7e082533aadc92283adb4373e5493cec54477
Mujahid Jaffer <mujahidrj@gmail.com>
2020-10-10 22:25:39 -0400

    Title and search bar now move up after button click

--- App.js
+++ App.js
@@ -1,42 +1,43 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, ImageBackground, View, SafeAreaView } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, ImageBackground, View, SafeAreaView } from 'react-native';
 
-import Title from "./components/Title";
-import Search from "./components/Search";
-import BookList from "./components/BookList";
-import { Container } from "native-base";
+import Title from './components/Title';
+import Search from './components/Search';
+import BookList from './components/BookList';
+import { Container } from 'native-base';
 
 function App() {
-    const [currentBooksList, setCurrentBooksList] = useState([]);
+	const [currentBooksList, setCurrentBooksList] = useState([]);
 
-    const onSearch = (booksArray) => {
-        setCurrentBooksList(booksArray);
-        console.log(currentBooksList);
-    };
-    return (
-        <Container>
-            <ImageBackground
-                style={styles.backgroundImage}
-                source={require("./assets/background-image.jpg")}
-            >
-                <SafeAreaView style={styles.safeArea}>
-                    <Title />
-                    <Search onSearch={onSearch} />
-                    <BookList />
-                </SafeAreaView>
-            </ImageBackground>
-        </Container>
-    );
+	const onSearch = (booksArray) => {
+		console.log(currentBooksList);
+		setCurrentBooksList(booksArray);
+	};
+
+	return (
+		<Container>
+			<ImageBackground
+				style={styles.backgroundImage}
+				source={require('./assets/background-image.jpg')}
+			>
+				<SafeAreaView style={styles.safeArea}>
+					<Title booksArray={currentBooksList} />
+					<Search onSearch={onSearch} />
+					<BookList />
+				</SafeAreaView>
+			</ImageBackground>
+		</Container>
+	);
 }
 
 const styles = StyleSheet.create({
-    backgroundImage: {
-        flex: 1,
-    },
-    safeArea: {
-        alignItems: "center",
-    },
+	backgroundImage: {
+		flex: 1,
+	},
+	safeArea: {
+		alignItems: 'center',
+	},
 });
 
 export default App;
--- components/Title.js
+++ components/Title.js
@@ -1,39 +1,43 @@
-//import { StatusBar } from "expo-status-bar";
-import * as Font from "expo-font";
-//import React from "react";
-import React, { useState } from "react";
-import { Text, View, StyleSheet } from "react-native";
-import { AppLoading } from "expo";
-import { processFontFamily, useFonts } from "expo-font";
-import { Inter_400Regular } from "@expo-google-fonts/inter";
+import * as Font from 'expo-font';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { AppLoading } from 'expo';
+
 const fetchFonts = () => {
-  return Font.loadAsync({
-    "ArchitectsDaughter-Regular": require("../assets/fonts/ArchitectsDaughter-Regular.ttf"),
-  });
+	return Font.loadAsync({
+		'ArchitectsDaughter-Regular': require('../assets/fonts/ArchitectsDaughter-Regular.ttf'),
+	});
 };
-function Title(props) {
-  const [dataloaded, setDataLoaded] = useState(false);
-  if (!dataloaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)}
-      />
-    );
-  }
-  return (
-    <View>
-      <Text style={styles.title}>All Things Books</Text>
-    </View>
-  );
+
+function Title({ booksArray }) {
+	const titleMessage =
+		booksArray.length > 0 ? (
+			<Text style={styles.titleOnSearch}>All Things Books</Text>
+		) : (
+			<Text style={styles.title}>All Things Books</Text>
+		);
+
+	const [dataloaded, setDataLoaded] = useState(false);
+	if (!dataloaded) {
+		return <AppLoading startAsync={fetchFonts} onFinish={() => setDataLoaded(true)} />;
+	}
+	return <View>{titleMessage}</View>;
 }
 
 const styles = StyleSheet.create({
-  title: {
-    justifyContent: "center",
-    marginTop: "60%",
-    fontFamily: "ArchitectsDaughter-Regular",
-    fontSize: 45,
-  },
+	title: {
+		justifyContent: 'center',
+		marginTop: '60%',
+		marginBottom: '2%',
+		fontFamily: 'ArchitectsDaughter-Regular',
+		fontSize: 45,
+	},
+	titleOnSearch: {
+		justifyContent: 'center',
+		marginTop: '10%',
+		marginBottom: '2%',
+		fontFamily: 'ArchitectsDaughter-Regular',
+		fontSize: 45,
+	},
 });
 export default Title;
